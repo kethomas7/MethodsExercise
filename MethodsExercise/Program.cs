be418baba6f35af96579f132b19e523407c60e36
Kaitlyn Thomas <kaitlynthomas2416@gmail.com>
2024-04-29 14:01:13 -0500

    Completed Exercise 1 and 2 for Methods Exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,57 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GetInfo();
+
+            int addResult = Add(7, 7);
+            int multiplyResult = Multiply(4, 6);
+            int divideResult = Divide(20, 4);
+            int subtractResult = Subtract(15,5);
+
+
+            Console.WriteLine($"Here are your math results:{addResult}, {multiplyResult}, {divideResult}, {subtractResult} ");
+            
         }
+        //Exercise 1
+        public static void GetInfo () 
+        {
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine("What's your favorite color?");
+            string color = Console.ReadLine();
+
+            Console.WriteLine("What's your favorite animal?");
+            string animal = Console.ReadLine();
+
+            Console.WriteLine("What's your favorite band?");
+            string band = Console.ReadLine();
+
+            Console.WriteLine($"Here's a review of the question above:\nName:{name}\nColor:{color}\nAnimal:{animal}\nBand:{band} ");
+        
+        }
+
+        //Exercise 2
+        public static int Add(int num1, int num2)
+        { 
+        return num1 + num2;
+        }
+
+        public static int Multiply(int num1, int num2) 
+        {
+        return (num1 * num2);
+        }
+
+        public static int Divide(int num1, int num2) 
+        { 
+        return (num1 / num2);
+        }
+        
+        public static int Subtract(int num1, int num2)
+        {
+         return num1 - num2;
+        }
+
+        
     }
 }
